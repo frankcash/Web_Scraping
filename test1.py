c04325bbadf574db687c889fca6a90ff6378badd
frankcash <cfcash@mtu.edu>
2013-12-14 18:52:59 -0500

    added a script to find what the user is looking for

--- test1.py
+++ test1.py
@@ -3,14 +3,16 @@ import urllib
 import urllib2
 import requests
 
-"""urlInput = raw_input("enter a url which you wish to find all the URLs on\n") #this adds an option for user input
-url = urlInput"""
-url = "http://www.python.org" 
+urlInput = raw_input("enter a url which you wish to find all the URLs on\n") #this adds an option for user input
+lookingFor = raw_input("what are you looking for?\n")
+url = urlInput
 content = urllib2.urlopen(url).read()
 soup = BeautifulSoup(content)
 for link in soup.find_all('a'):
     print(link.get('href'))#displays the URLS
     
-"""print(soup.title.string)#reads the title"""
-print(soup.find_all(text=re.compile('Python')))#will display everything inolving python
+
+print(soup.find_all(text=re.compile(lookingFor)))#will display everything inolving python
+
+
 
