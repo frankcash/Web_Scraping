1a26b2ae28432b94b99cbed9cb5719bef0639a3c
frankcash <cfcash@mtu.edu>
2013-12-14 14:23:00 -0500

    changed the readme as well as added another line of code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Personal project based upon web scraping that I am working on.  Will include a lot of files (potentially).
+
+Somethings may be commented so they don't run, but anyone can uncomment them and run them.  I just commented them because they aren't necessary for the part of the code that I am testing/ working on and thus greatly decrease the output.  
--- test1.py
+++ test1.py
@@ -3,17 +3,14 @@ import urllib
 import urllib2
 import requests
 
-"""urlInput = raw_input("enter a url which you wish to find all the URLs on\n")
+"""urlInput = raw_input("enter a url which you wish to find all the URLs on\n") #this adds an option for user input
 url = urlInput"""
-url = "http://www.python.org"
+url = "http://www.python.org" 
 content = urllib2.urlopen(url).read()
 soup = BeautifulSoup(content)
 for link in soup.find_all('a'):
-    print(link.get('href'))#reads the links
+    print(link.get('href'))#displays the URLS
     
 """print(soup.title.string)#reads the title"""
-
-
-print(soup.find_all(text="python"))
-
+print(soup.find_all(text=re.compile('Python')))#will display everything inolving python
 
