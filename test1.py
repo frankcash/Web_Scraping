bad38545648940087dcb206a50f3cfc75d9e7cb2
frankcash <cfcash@mtu.edu>
2013-12-20 12:10:27 -0500

    Forget to import the .re library

--- test1.py
+++ test1.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib2
 import requests
+import re
 
 urlInput = raw_input("enter a url which you wish to find all the URLs on\n") #this adds an option for user input
 lookingFor = raw_input("what are you looking for?\n")
@@ -12,6 +13,7 @@ for link in soup.find_all('a'):
     print(link.get('href'))#displays the URLS
     
 print(soup.title.string)#reads the title
+
 print(soup.find_all(text=re.compile(lookingFor)))#will display everything inolving python
 
 
