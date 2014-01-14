5dd70f187fdd424aba06469b565b9d5739b07d22
frankcash <cfcash@mtu.edu>
2014-01-13 21:27:34 -0500

    changed the program to allow user to input, need to add a way to pull result table

--- test2.py
+++ test2.py
@@ -1,15 +1,12 @@
 #makes use of Michigan Tech's Directory
-
+name = raw_input("Enter the first name of the person you wish to look up: ")
 from splinter import Browser
 with Browser() as browser:
     url = "https://www.mtu.edu/mtuldapweb/web_lookup/"
     browser.visit(url)
-    browser.fill('advtext', 'Frank')
+    browser.fill('advtext', name)
     #find and click the 'search' button
     button = browser.find_by_name('submit')
     #interact with elements
     button.click()
-    if browser.is_text_present('fjkampe'):
-        print "True"
-    else:
-        print "False"
+    raw_input("")#gives a way for me to observe
