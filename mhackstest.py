4bbf952163d47106294680bea450c2954c25e95c
frankcash <cfcash@mtu.edu>
2014-01-18 14:38:20 -0500

    made it so browser will enter infinite loop allowing, the user to send as many queries as they want.

--- mhackstest.py
+++ mhackstest.py
@@ -1,12 +1,27 @@
 #main file
+from timeit import default_timer
+start = default_timer()
+duration = default_timer()-start
+minutes=duration/60
+
 print "Welcome to BrowseScape.."
-input = raw_input("")
+print "Current commands include weather, wikipedia, search, and exit."
 
-if input == "weather":
-	import weatherTest
 
-elif input == "wikipedia":
-	import wikipediaAPI
 
-elif input == "exit":
-	exit()
+while minutes<1000:
+	input=raw_input("")
+	if input == "weather":
+		import weatherTest
+
+	elif input == "wikipedia":
+		import wikipediaAPI
+
+	elif input == "search":
+		import test1
+
+	elif input == "exit":
+		exit()
+
+
+
--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ import requests
 import re
 
 
-urlInput = raw_input("enter a url which you wish to find all the URLs on\n") #this adds an option for user input
+urlInput = raw_input("enter the page you wish to go to\n") #this adds an option for user input
 lookingFor = raw_input("what are you looking for?\n")
 
 
@@ -13,10 +13,7 @@ def runSoup(urlInput, lookingFor):#defines the function
     url = urlInput
     content = urllib2.urlopen(url).read()
     soup = BeautifulSoup(content)
-    print("The title of this page is "+soup.title.string+"\n")#reads the title
-    print ("This page links to:\n")
-    for link in soup.find_all('a'):
-        print(link.get('href'))#displays the URLS  
+    print("The title of this page is "+soup.title.string+"\n")#reads the title 
     print ("\n")
     print("Your Search Returned"+ "\n")
     results=(soup.findAll(text=re.compile(lookingFor)))#will display everything inolving python
