c240e39d3131034d2fb58ad0459dfdbcef9e91fa
frankcash <cfcash@mtu.edu>
2014-01-29 14:04:24 -0500

    updated README

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-Personal project based upon web scraping that I am working on.  Will include a lot of files (potentially).
+BrowseScrape
+-------------------------------------------------------------
+Personal project based upon web scraping that I am working on.  To act as a web browser for minimalistic browsing.  Utilizes web scraping and APIs.
 
-Somethings may be commented so they don't run, but anyone can uncomment them and run them.  I just commented them because they aren't necessary for the part of the code that I am testing/ working on and thus greatly decrease the output.  
+
+-------------------------------------------------------------
+The required librarys and APIs are easily installed via pip.
+
+Requires BeautifulSoup4
 
 Requires Python Weather API http://code.google.com/p/python-weather-api/
+
+Requires Splinter http://splinter.cobrateam.info/docs/contribute.html
+
+Requires Wikipedia API https://github.com/goldsmith/Wikipedia
+-------------------------------------------------------------
+
+Code written for testing can be given upon further notice.
--- weatherTest.py
+++ weatherTest.py
@@ -1,4 +1,47 @@
-#Generates Weather conditions utilizing the Python-Weather-API 
+#Generates Weather conditions utilizing the Python-Weather-API
+def usrInput():
+    global weather
+    global displayTempIn
+    import pywapi, pprint, string, time
+    location = raw_input("Which ZIP code would you like to know the weather for? \n")
+    displayTempIn = raw_input("Would you like the Temperature to be displayed in Celsius or Fahrenheit? \n")
+    p = pprint.PrettyPrinter(indent=4)
+    weather = pywapi.get_weather_from_yahoo(location)
+    displayTempIn=displayTempIn.lower()#converts to lower case to prevent any errors from occuring
+    return weather;
+usrInput()
+    # following code converts temperature from C to F
+
+def CtoF(weather):
+    global ftemp #declares ftemp to be a global variable
+    temp = weather['condition']['temp']
+    FTemp=int(float(temp)) # Converting from string to int to do math
+    FTemp=FTemp*9
+    FTemp=FTemp/5
+    Ftemp=FTemp+32
+    F=Ftemp
+    ftemp=str(F) #converts from int to str so it can be output
+    return;
+
+CtoF(weather)
+
+def displayResults(ftemp, weather):
+    print("Outside Conditions Appear To Be "+weather['condition']['text'])#tells current conditions
+    if displayTempIn == "celsius":
+        print("The Temperature Is "+weather['condition']['temp']+u"\N{DEGREE SIGN}"+"C")#tells temperature in celsius
+    elif displayTempIn == "fahrenheit":
+        print("The Temperature Is "+ftemp+u"\N{DEGREE SIGN}"+"F")#tells temperature in fahrenheit
+    else:  #if response was incromprehensible the display will default to both fahrenheit and celsius
+        print("The Temperature Is "+weather['condition']['temp']+u"\N{DEGREE SIGN}"+"C")#tells temperature in celsius
+        print("The Temperature Is "+ftemp+u"\N{DEGREE SIGN}"+"F")#tells temerature in fahrenheit
+    print("Last Updated At "+weather['condition']['date'])#prints when last updated
+displayResults(ftemp, weather)
+
+
+"""
+weather_com isn't working currently, need to look into issue
+
+#Generates Weather conditions utilizing the Python-Weather-API
 import pywapi, pprint, string, time
 location = raw_input("Which ZIP code would you like to know the weather for? \n")
 displayTempIn = raw_input("Would you like the Temperature to be displayed in Celsius or Fahrenheit? \n")
@@ -12,15 +55,15 @@ displayTempIn=displayTempIn.lower()#converts to lower case to prevent any errors
 
 
 def CtoF(weather):
-	global ftemp #declares ftemp to be a global variable
-	temp = weather['current_conditions']['temperature']
-	FTemp=int(float(temp)) # Converting from string to int to do math
-	FTemp=FTemp*9
-	FTemp=FTemp/5
-	Ftemp=FTemp+32
-	F=Ftemp
-	ftemp=str(F) #converts from int to str so it can be output
-	return;
+        global ftemp #declares ftemp to be a global variable
+        temp = weather['current_conditions']['temperature']
+        FTemp=int(float(temp)) # Converting from string to int to do math
+        FTemp=FTemp*9
+        FTemp=FTemp/5
+        Ftemp=FTemp+32
+        F=Ftemp
+        ftemp=str(F) #converts from int to str so it can be output
+        return;
 CtoF(weather)
 
 
@@ -30,7 +73,7 @@ def displayResults(ftemp, weather):
         print("The Temperature Is "+weather['current_conditions']['temperature']+u"\N{DEGREE SIGN}"+"C")#tells temperature in celsius
     elif displayTempIn == "fahrenheit":
         print("The Temperature Is "+ftemp+u"\N{DEGREE SIGN}"+"F")#tells temperature in fahrenheit
-    else:  #if response was incromprehensible the display will default to both fahrenheit and celsius
+    else: #if response was incromprehensible the display will default to both fahrenheit and celsius
         print("The Temperature Is "+weather['current_conditions']['temperature']+u"\N{DEGREE SIGN}"+"C")#tells temperature in celsius
         print("The Temperature Is "+ftemp+u"\N{DEGREE SIGN}"+"F")#tells temerature in fahrenheit
     print("Last Updated At "+weather['current_conditions']['last_updated'])#prints when last updated
@@ -42,4 +85,4 @@ displayResults(ftemp, weather)
 #print ("The High For Tomorrow Is "+weather['forecasts'][1]['high']+u"\N{DEGREE SIGN}"+"C")#tells tomorrow's high
 
 
-
+"""
